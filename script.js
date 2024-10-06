91d0cbf5e7aed33102252caee3b4f88922721b56
Helgrindi <v.decoen@gmail.com>
2024-10-06 20:39:27 +0200

    Created input field which replaces grid with given grid, doesn't run when input is > 100 or not a number, prompts again until valid.

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 const container = document.querySelector("#container");
 
 function createSquares(num) {
+    let percentage = (100 / num) + "%";
     num = num * num;
-    console.log(num);
 
     for (let i = 0; i < num; i++) {
         const square = document.createElement("div");
         square.className = "square";
+        square.style.flex = `0 0 ${percentage}`;
         container.appendChild(square);
 
         square.addEventListener("mouseenter", () => {
-            square.style.backgroundColor = "rgb(80, 80, 80)";
+            square.style.backgroundColor = "#8E8E93";
         });
     }
 }
-
 createSquares(16);
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    let amount = prompt("Create a new grid, choose the number of squares per side (Maximum 100) :");
+    if (amount === null) return;
+
+    console.log(amount);
+
+    while (amount > 100 || isNaN(amount)) {
+        amount = prompt("Invalid number, please choose one up to and including 100:");
+        console.log(amount);
+    }
+
+    container.innerHTML = "";
+    createSquares(amount);
+});
